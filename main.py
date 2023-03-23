d7389afff588b95dbf5f4c40d60a94746c5a50dd
XoqpoX <kirill78787@yandex.ru>
2023-03-23 20:22:42 +0300

    3

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ player_list = {}
 admin_sid = None
 role10 = 6 * ['Мирный житель'] + 2 * ['Мафия'] + ['Шериф'] + ['Дон мафии']
 role8 = 5 * ['Мирный житель'] + 1 * ['Мафия'] + ['Шериф'] + ['Дон мафии']
+role = role8
 maf_targets = []
 maf_target = None
 
@@ -30,7 +31,7 @@ def disconnect(sid):
 def message(sid, data):
     global player_list, admin_sid
 
-    print('Сообщение от клиента {}: {}'.format(sid, data))
+    print('Сообщение message от клиента {}: {}'.format(sid, data))
     # when nickname entering
     if len(data) == 1 and 'username' in data and isinstance(data['username'], str) and len(player_list) < 10:
         if admin_sid is None:  # first entered - admin
@@ -45,7 +46,8 @@ def message(sid, data):
 
 @sio.event
 def start(sid, data):
-    global player_list
+    print('Сообщение start от клиента {}: {}'.format(sid, data))
+    global player_list, role
 
     if data == 'start':
         # shuffle order of players
@@ -57,11 +59,12 @@ def start(sid, data):
             player['number'] = num
             shuffled_player_list[key] = player
         # choose 8 players or 10
-        if len(shuffled_player_list) == 8:
+        if len(shuffled_player_list) <= 8:
             role = role8
+            random.shuffle(role)
         elif len(shuffled_player_list) == 10:
             role = role10
-        random.shuffle(role)
+            random.shuffle(role)
         # giving roles to players
         for sid, data in shuffled_player_list.items():
             data['role'] = role.pop()
@@ -75,24 +78,34 @@ def start(sid, data):
 
 @sio.event
 def kill(sid, data):
+    print('Сообщение kill от клиента {}: {}'.format(sid, data))
     global player_list, maf_targets, maf_target
 
-    if len(player_list) == 8:
+    if len(player_list) <= 8:
         maf_count = 2
     elif len(player_list) == 10:
         maf_count = 3
 
-    if len(maf_targets) < maf_count and type(data) == int:  # waiting for all shots and check for them data type
-        maf_targets += data
-    if len(maf_targets) == maf_count and maf_targets[0] != 0:  # when shot count is equal to mafia count,and it is not 0
+    print('len = ', len(player_list))
+    print('maf_count = ', maf_count)
+    print('maf_targets do = ', maf_targets)
+    if len(maf_targets) < maf_count:  # waiting for all shots
+        maf_targets.append(data)
+        print('maf_targets posle = ', maf_targets)
+    if len(maf_targets) == maf_count:  # when shot count is equal to mafia count
         if all(x == maf_targets[0] for x in maf_targets):  # if shots are same
-            maf_target = maf_targets[0]
+            if maf_targets[0] != 0:
+                maf_target = maf_targets[0]
+            else:
+                maf_target = '0'
         else:
-            maf_target = 0
-    if type(data) != int:  # print error if data type is not int
-        print('incorrect kill data type')
+            maf_target = '0'
     if maf_target is not None:  # send maf_target if it is not None
         sio.emit('chat_message', maf_target, room='room')
+        # reset
+        maf_count = 2
+        maf_targets = []
+        maf_target = None
 
 
 # Запускаем сервер Socket.IO
